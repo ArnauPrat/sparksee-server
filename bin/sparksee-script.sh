b248274d23dcb1d480e0cfda667ffb84bdb8c98e
mikemaal <mike.aguila.lorente@gmail.com>
2015-05-14 14:39:25 +0200

    dependencies

--- bin/sparksee-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-file="$1"
-locale="$2"
-query="{\"gremlin\":\"g.runScript(script,locale)\",\"bindings\":{\"script\":\"$file\",\"locale\":\"$locale\"}}"
-echo $query
-
-curl -X POST -H "Accept: application/json"  -d $query http://localhost:8182
--- bin/sparksee-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-(cd `dirname $0`/../gremlin-server/ && bin/sparksee-server.sh $@)
