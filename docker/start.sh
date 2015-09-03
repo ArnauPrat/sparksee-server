4db9bd6a29ee6fa1614976fb0065fa541a9e8399
rpau <rpau@ac.upc.edu>
2015-09-03 19:15:06 +0200

    docker errors solved

--- docker/start.sh
+++ docker/start.sh
@@ -1,34 +1,43 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/.m2
+touch $HOME/.m2/settings.xml
+
+echo "<settings><servers>" >> $HOME/.m2/settings.xml
+echo "<server><id>adapt03-libs</id><username>" >> $HOME/.m2/settings.xml
+echo $MVN_USER >> $HOME/.m2/settings.xml
+echo "</username><password>" >> $HOME/.m2/settings.xml
+echo $MVN_PWD >> $HOME/.m2/settings.xml
+echo "</password></server>" >> $HOME/.m2/settings.xml
 
 
-echo "<settings><servers>" > $HOME/.m2/settings.xml
-echo "<server><id>adapt03-libs</id><username>" > $HOME/.m2/settings.xml
-echo $MVN_USER > $HOME/.m2/settings.xml
-echo "</username><password>" $HOME/.m2/settings.xml
-echo $MVN_PWD > $HOME/.m2/settings.xml
-echo "</password></server>"
+echo "<server><id>adapt03</id><username>" >> $HOME/.m2/settings.xml
+echo $MVN_USER >> $HOME/.m2/settings.xml
+echo "</username><password>" >> $HOME/.m2/settings.xml
+echo $MVN_PWD >> $HOME/.m2/settings.xml
+echo "</password></server>" >> $HOME/.m2/settings.xml
 
 
-echo "<server><id>adapt03</id><username>" > $HOME/.m2/settings.xml
-echo $MVN_USER > $HOME/.m2/settings.xml
-echo "</username><password>" $HOME/.m2/settings.xml
-echo $MVN_PWD > $HOME/.m2/settings.xml
-echo "</password></server>"
+echo "<server><id>adapt03-coherentpaas-snapshots</id><username>" >> $HOME/.m2/settings.xml
+echo $MVN_USER >> $HOME/.m2/settings.xml
+echo "</username><password>" >> $HOME/.m2/settings.xml
+echo $MVN_PWD >> $HOME/.m2/settings.xml
+echo "</password></server>" >> $HOME/.m2/settings.xml
+echo "</servers></settings>" >> $HOME/.m2/settings.xml
 
+echo "building the project"
 
-echo "<server><id>adapt03-coherentpaas-snapshots</id><username>" > $HOME/.m2/settings.xml
-echo $MVN_USER > $HOME/.m2/settings.xml
-echo "</username><password>" $HOME/.m2/settings.xml
-echo $MVN_PWD > $HOME/.m2/settings.xml
-echo "</password></server>"
-echo "</servers></settings>"> $HOME/.m2/settings.xml
+cd /home/root/sparksee && mvn package -DskipTests
 
-cd /home/root/sparksee && mvn package
+echo "copying the standlalone into the working dir"
 
-cp -R /home/root/sparksee/gremlin-server /usr/local/sparksee
+cp -R /home/root/sparksee/gremlin-server/* /usr/local/sparksee
 
-cp /home/root/sparksee/docker/gremlin-server-rest-sparksee.yaml target/gremlin-server-3.0.0-SNAPSHOT-standalone/conf
+sed  "1 i host: $HOSTNAME" /home/root/sparksee/docker/gremlin-server-rest-sparksee.yaml
 
-cp /home/root/sparksee/docker/sparksee.cfg target/gremlin-server-3.0.0-SNAPSHOT-standalone/conf/sparksee.cfg
+cp /home/root/sparksee/docker/gremlin-server-rest-sparksee.yaml /usr/local/sparksee/target/gremlin-server-3.0.0-SNAPSHOT-standalone/conf/gremlin-server-rest-sparksee.yaml
+
+cp /home/root/sparksee/docker/sparksee.cfg /usr/local/sparksee/target/gremlin-server-3.0.0-SNAPSHOT-standalone/conf/sparksee.cfg
 
 export GREMLIN_SERVER_HOME=/usr/local/sparksee/target/gremlin-server-3.0.0-SNAPSHOT-standalone
 
@@ -36,6 +45,7 @@ export PATH=$PATH:$GREMLIN_SERVER_HOME/bin
 
 cd /usr/local/sparksee/target/gremlin-server-3.0.0-SNAPSHOT-standalone
 
+echo "starting the gremlin server"
+
 gremlin-server.sh $GREMLIN_SERVER_HOME/conf/gremlin-server-rest-sparksee.yaml
 
-tail -f gremlin-server.log
