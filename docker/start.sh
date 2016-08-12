a540690cd8c624a408034e00da141b77f7a68062
mikemaal <mike.aguila.lorente@gmail.com>
2016-08-12 12:20:21 +0200

    start by command no by default

--- docker/start.sh
+++ docker/start.sh
@@ -2,5 +2,5 @@
 
 if [ -n "$XRAYHOST" ]; then sed  "s/REPLACE_XRAY_HOST/${XRAYHOST}/g" /home/root/sparksee/docker/logback.template.groovy > /usr/local/sparksee/etc/logback.groovy; fi
 
-cd $SPARKSEE_SERVER_HOME && sparksee -Dsparksee.license=$SPARKSEE_LICENSE -Dsparksee.io.recovery=true
+cd $SPARKSEE_SERVER_HOME && sparksee start -Dsparksee.license=$SPARKSEE_LICENSE -Dsparksee.io.recovery=true
 
